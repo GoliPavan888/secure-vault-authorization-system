591e7b9cab1796a14f6661b38c481c8ff070a2be
Pavan Kumar Goli <golipavan888@gmail.com>
2025-12-27 13:44:49 +0530

    Fix RPC URL to use blockchain service

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,7 +4,7 @@ module.exports = {
   solidity: "0.8.20",
   networks: {
     localhost: {
-      url: process.env.RPC_URL || "http://127.0.0.1:8545"
+      url: "http://blockchain:8545"
     }
   }
 };
