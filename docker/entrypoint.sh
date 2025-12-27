a2e21aafb7c20896d5a116d45f9fc0691215428b
Pavan Kumar Goli <golipavan888@gmail.com>
2025-12-27 13:41:37 +0530

    Install netcat for RPC wait loop

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
 set -e
 
-echo "Waiting for blockchain RPC..."
-until nc -z blockchain 8545; do
-  sleep 1
-done
-
-echo "Blockchain RPC ready"
+echo "Waiting for blockchain startup..."
+sleep 6
 
 npx hardhat run scripts/deploy.js --network localhost
 
